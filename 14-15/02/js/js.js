8d84d4cc291eb962dc29295e64c934c35af6f0f6
ksuka <ksikacheva@yandex.ru>
2025-09-10 16:38:08 +0300

    pasport

---
+++ 14-15/02/js/js.js
@@ -0,0 +1,15 @@
+function find_edit(){
+    const item  = document.getElementById('surname')
+    item.innerHTML="<b>Sikacheva</b>"
+
+    const item2  = document.getElementById('name')
+    item2.innerHTML="<b>Kseniya</b>"
+
+    const item3  = document.getElementById('year')
+    item3.innerHTML="<b>2006</b>"
+}
+
+
+
+const node_for_click = document.getElementById("for_click")
+node_for_click.addEventListener("click", find_edit)
