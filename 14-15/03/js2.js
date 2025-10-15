27f91b9e7363a8dd060e3b77591ed4d0e5cc4419
ksuka <ksikacheva@yandex.ru>
2025-10-15 13:09:13 +0300

    fix

--- 14-15/03/js2.js
+++ 14-15/03/js2.js
@@ -1,6 +1,50 @@
+
+
+
+
+function get_result(){
+console.log("S, R, K");
+let S = parseInt(elementS.value);
+let R = parseInt(elementR.value);
+let K = parseInt(elementK.value);
+console.log(S, R, K)
+
+let a = Math.sqrt(S);
+
+let valid, notValid;
+if (a>= 2*(R+K) && R>0) {R=valid}
+else {R=notValid}
+
+if ((a - 2*R) >=K ) {
+    result = "Да, можно";
+    check= true;
+}
+ 
+else{
+    result = "Нет, нельзя";
+    check= false;
+}
+
+
+function send() {
+    if (check) {
+        document.getElementById("UserEnter").submit();
+    } else {
+        alert("Есть недостатки. Повторите ввод")
+    }
+}
+
+
+}
+
+
 let result;
 let check;
 
-let elementA = document.getElementById("a");
-let elementB = document.getElementById("b");
-let elementC = document.getElementById("c");
+let elementS = document.getElementById("S");
+let elementR = document.getElementById("R");
+let elementK = document.getElementById("K");
+
+let verify = document.getElementById("verify");
+console.log(verify.value)
+verify.addEventListener('click', get_result)
