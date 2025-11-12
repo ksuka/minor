abb6b77c1a89155fa16399eadb695f6d71c6c217
ksuka <ksikacheva@yandex.ru>
2025-11-12 12:51:06 +0300

    2 module

--- 14-15/03/js2.js
+++ 14-15/03/js2.js
@@ -1,42 +1,33 @@
-
-
-
-
 function get_result(){
-console.log("S, R, K");
-let S = parseInt(elementS.value);
-let R = parseInt(elementR.value);
-let K = parseInt(elementK.value);
-console.log(S, R, K)
+    console.log("S, R, K");
+    let S = parseInt(elementS.value);
+    let R = parseInt(elementR.value);
+    let K = parseInt(elementK.value);
+    console.log(S, R, K);
 
-let a = Math.sqrt(S);
+    let side = Math.sqrt(S); 
+    
 
-let valid, notValid;
-if (a>= 2*(R+K) && R>0) {R=valid}
-else {R=notValid}
+    let canFit = (2 * R + 2 * K) <= side;
+    
+    if (canFit) {
+        result = "Да, можно";
+        check = true;
+    } else {
+        result = "Нет, нельзя";
+        check = false;
+    }
 
-if ((a - 2*R) >=K ) {
-    result = "Да, можно";
-    check= true;
+    document.getElementById("result").value = result;
 }
- 
-else{
-    result = "Нет, нельзя";
-    check= false;
-}
-
 
 function send() {
-    if (check) {
+    if (check === true) { 
         document.getElementById("UserEnter").submit();
     } else {
-        alert("Есть недостатки. Повторите ввод")
+        alert("Есть недостатки. Повторите ввод");
     }
 }
-
-
-}
-
 
 let result;
 let check;
@@ -46,5 +37,7 @@ let elementR = document.getElementById("R");
 let elementK = document.getElementById("K");
 
 let verify = document.getElementById("verify");
-console.log(verify.value)
-verify.addEventListener('click', get_result)
+verify.addEventListener('click', get_result);
+
+const sendbtn = document.getElementById('send');
+sendbtn.addEventListener('click', send);
